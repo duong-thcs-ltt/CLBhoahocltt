b5514c6c6356df544ba580071cd789b46e5ea6a0
duong-thcs-ltt <quachlong220666@gmail.com>
2026-09-04 15:42:00 +0700

    Fix serverless import for Vercel

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import type { Request, Response } from "express";
 import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv";
-import { normalizeChemistryText } from "./src/utils/chemistryFormatter";
+import { normalizeChemistryText } from "./src/utils/chemistryFormatter.ts";
 
 dotenv.config();
 
